254b26ae3ceb65914fc9e9554d14fc79dd24cc20
Heather Miller <heather.miller114@gmail.com>
2019-01-09 18:02:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do
     @phrase = params[:phrase]
-    params[:number].to_f.times do
+    params[:number].to_i.times do
       puts "#{@phrase}"
     end
   end
